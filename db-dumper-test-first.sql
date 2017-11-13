1e57a3f6970742b24c378efcca5a7b7d0b3f2145
Soufian Zaouam <soufian.zaouam@orange.com>
2017-11-13 17:10:29 +0100

    Create db-dumper-test-first.sql

---
+++ db-dumper-test-first.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS dbdumper_first_test;
+create table dbdumper_first_test (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (1, 'Doreen', 'Ingerson', 'dingerson0@unicef.org', 'Female', '232.230.217.151');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (2, 'Clemmie', 'Fiennes', 'cfiennes1@istockphoto.com', 'Male', '213.132.212.2');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (3, 'Lia', 'Bricket', 'lbricket2@boston.com', 'Female', '185.30.157.113');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (4, 'Wake', 'Lawey', 'wlawey3@soundcloud.com', 'Male', '153.206.69.245');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (5, 'Rusty', 'Flohard', 'rflohard4@digg.com', 'Male', '166.241.165.222');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (6, 'Betteanne', 'Scawn', 'bscawn5@oaic.gov.au', 'Female', '96.186.147.160');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (7, 'Ram', 'Relton', 'rrelton6@purevolume.com', 'Male', '210.254.107.40');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (8, 'Marc', 'Oliveira', 'moliveira7@nymag.com', 'Male', '72.138.178.175');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (9, 'Myrta', 'Moine', 'mmoine8@paginegialle.it', 'Female', '130.195.93.228');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (10, 'Inglis', 'Shergill', 'ishergill9@behance.net', 'Male', '62.151.14.65');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (11, 'Launce', 'O''Riordan', 'loriordana@de.vu', 'Male', '232.72.110.9');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (12, 'Devonne', 'Blew', 'dblewb@cocolog-nifty.com', 'Female', '66.51.40.137');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (13, 'Gerhardt', 'Stidever', 'gstideverc@si.edu', 'Male', '52.63.179.134');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (14, 'Ilario', 'Estcourt', 'iestcourtd@free.fr', 'Male', '17.151.237.3');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (15, 'Marcelia', 'Iacomo', 'miacomoe@chron.com', 'Female', '50.17.63.118');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (16, 'Ode', 'Allone', 'oallonef@usa.gov', 'Male', '159.216.154.83');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (17, 'Donal', 'Newlands', 'dnewlandsg@delicious.com', 'Male', '75.85.209.55');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (18, 'Becka', 'Goodbourn', 'bgoodbournh@hexun.com', 'Female', '150.202.97.110');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (19, 'Derrik', 'Tremoulet', 'dtremouleti@gov.uk', 'Male', '38.220.181.91');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (20, 'Johna', 'Caillou', 'jcaillouj@xinhuanet.com', 'Female', '109.197.86.188');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (21, 'Joanna', 'Votier', 'jvotierk@businesswire.com', 'Female', '16.234.39.54');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (22, 'Cari', 'Ludwell', 'cludwelll@bbc.co.uk', 'Female', '200.116.189.29');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (23, 'Bertram', 'Gathercoal', 'bgathercoalm@princeton.edu', 'Male', '72.14.111.216');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (24, 'Clementius', 'Daughtry', 'cdaughtryn@washington.edu', 'Male', '14.130.229.184');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (25, 'Marcus', 'Rediers', 'mredierso@nature.com', 'Male', '133.108.174.149');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (26, 'Randy', 'Skea', 'rskeap@typepad.com', 'Male', '0.126.181.87');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (27, 'Moritz', 'Guyers', 'mguyersq@theguardian.com', 'Male', '131.86.218.146');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (28, 'Langsdon', 'Kalinke', 'lkalinker@earthlink.net', 'Male', '23.148.245.240');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (29, 'Casar', 'Ilott', 'cilotts@wiley.com', 'Male', '90.34.94.84');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (30, 'Helena', 'Lambal', 'hlambalt@cbslocal.com', 'Female', '192.173.116.88');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (31, 'Ilse', 'Bader', 'ibaderu@princeton.edu', 'Female', '138.147.105.41');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (32, 'Marilin', 'Leverette', 'mleverettev@smugmug.com', 'Female', '97.53.100.58');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (33, 'Ianthe', 'Bardwell', 'ibardwellw@illinois.edu', 'Female', '49.161.205.69');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (34, 'Merci', 'Vasichev', 'mvasichevx@g.co', 'Female', '198.101.113.34');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (35, 'Emmy', 'Buzza', 'ebuzzay@dedecms.com', 'Male', '216.94.31.187');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (36, 'Marietta', 'Suckling', 'msucklingz@flickr.com', 'Female', '55.13.81.243');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (37, 'Pat', 'Joscelyne', 'pjoscelyne10@aboutads.info', 'Male', '228.201.193.147');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (38, 'Darby', 'Kording', 'dkording11@tripadvisor.com', 'Male', '24.204.213.64');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (39, 'Dotti', 'Chivrall', 'dchivrall12@csmonitor.com', 'Female', '76.174.220.247');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (40, 'Zebadiah', 'Roskeilly', 'zroskeilly13@google.pl', 'Male', '15.189.62.39');
+insert dbdumper_first_test (id, first_name, last_name, email, gender, ip_address) values (41, 'Nonnah', 'Butterick', 'nbutterick14@apache.org', 'Female', '207.219.100.93');
